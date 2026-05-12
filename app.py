bb65184768bff84f9a7535768d158dac3ede7fb1
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-13 00:16:41 +0530

    modified

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import Flask, jsonify
-from flask import request   # Add this to the imports
+from flask import request  # Add this to the imports
 
 app = Flask(__name__)
 
 # In-memory list of tasks (instead of database)
-tasks = [
-    {"id": 1, "title": "Learn APIs", "done": False}
-]
+tasks = [{"id": 1, "title": "Learn APIs", "done": False}]
 
-@app.route('/tasks', methods=['GET'])
+
+@app.route("/tasks", methods=["GET"])
 def get_tasks():
     return jsonify(tasks)  # Returns JSON
 
-@app.route('/tasks', methods=['POST'])
-def create_task():
-    new_task=request.get_json() # Get JSON from request body
-    new_task['id']=len(tasks)+1  # Simple ID
-    new_task['done']=False  # Default
-    tasks.append(new_task) # save it the tasks
-    return jsonify(new_task),201 # Return it + status 201
 
-if __name__ == '__main__':
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    new_task = request.get_json()  # Get JSON from request body
+    new_task["id"] = len(tasks) + 1  # Simple ID
+    new_task["done"] = False  # Default
+    tasks.append(new_task)  # save it the tasks
+    return jsonify(new_task), 201  # Return it + status 201
+
+
+if __name__ == "__main__":
     app.run(debug=True)
